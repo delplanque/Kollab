7d9f01668f49a373648720f882dbf9c5222cd685
Jordan DELPLANQUE <jdelplanque@access-it.fr>
2019-10-07 13:38:54 +0200

    header + drawer

--- src/features/home/Index.vue
+++ src/features/home/Index.vue
@@ -2,6 +2,32 @@
   <section>
     <div >
       <h1>Kollab</h1>
+      <v-layout
+    wrap
+    style="height: 200px;"
+  >
+    <v-container>
+      <v-layout justify-center>
+        <v-btn
+          color="pink"
+          dark
+          @click.stop="drawer = !drawer"
+        >
+          Toggle
+        </v-btn>
+      </v-layout>
+    </v-container>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+     <v-avatar>
+        <img src="https://randomuser.me/api/portraits/men/84.jpg">
+     </v-avatar>
+    </v-navigation-drawer>
+  </v-layout>
     </div>
   </section>
 </template>
@@ -12,6 +38,11 @@ export default {
 
   data() {
     return {
+      drawer: null,
+        items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' }
+        ]
     };
   },
 
