ca118fbce1e76f7dfe360cecb7f6d97e7f36f6df
mehechi <maxime1jacquet@gmail.com>
2019-10-08 16:26:12 +0200

    fix(active onglet)

--- src/features/toolbox/toolbox.vue
+++ src/features/toolbox/toolbox.vue
@@ -1,6 +1,13 @@
 <template>
   <section class="toolbox">
-    <div class="toolbox__onglet container">aef</div>
+    <ul class="toolbox__onglet container">
+      <li
+        :class="{ active: isActive('collaboratif') }"
+        @click="changeTab('collaboratif')"
+      >Collaboratifs</li>
+      <li :class="{ active: isActive('officiel') }" @click="changeTab('officiel')">Officiels</li>
+      <li :class="{ active: isActive('add') }" @click="changeTab('add')">Mes Ajouts</li>
+    </ul>
     <div class="toolbox__content">
       <div class="container">
         <div class="toolbox__list">
@@ -20,6 +27,7 @@ export default {
 
   data() {
     return {
+      tabactive: 'collaboratif',
       toolbox: [
         {
           name: '1',
@@ -43,14 +51,39 @@ export default {
 
   mounted() {},
 
-  methods: {}
+  methods: {
+    changeTab(tab) {
+      this.tabactive = tab;
+    },
+
+    isActive(tab) {
+      return this.tabactive === tab;
+    }
+  }
 };
 </script>
 
 
 <style scoped lang="scss">
 .toolbox {
+  padding-top: 50px;
+
   &__onglet {
+    display: flex;
+
+    li {
+      border-bottom: 2px solid transparent;
+      padding: 10px;
+      cursor: pointer;
+      margin: 0 20px 0 0;
+      text-align: center;
+      color: #666;
+
+      &.active {
+        border-bottom: 2px solid #4caf50;
+        color: #4caf50;
+      }
+    }
   }
   &__content {
     background-color: #f6f6f6;
