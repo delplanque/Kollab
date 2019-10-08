592c040b9169c9f5644d1f3f8a36c7d0b086c78f
mehechi <maxime1jacquet@gmail.com>
2019-10-08 15:31:19 +0200

    fix(store import)

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
-import Vue from "vue";
+import Vue from 'vue';
 
-import VueFire from "vuefire";
+import VueFire from 'vuefire';
 
-import VueResource from "vue-resource";
+import VueResource from 'vue-resource';
 
-import App from "./core/App.vue";
-import { database }     from './core/database';
-import { global } from "./core/environments";
-import router from "./core/router";
-import { store } from "./store/store";
+import App from './core/App.vue';
+import { database } from './core/database';
+import { global } from './core/environments';
+import router from './core/router';
 // import vuetify from './plugins/vuetify';
 
 Vue.use(VueResource);
@@ -18,10 +17,6 @@ Vue.config.environments = global.environments;
 
 new Vue({
   router,
-
   database,
-  store,
-
-  // vuetify,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
