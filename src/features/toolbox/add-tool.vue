f51ea754295518cf420be7b68faee723c9dddfd5
mehechi <maxime1jacquet@gmail.com>
2019-10-09 11:39:50 +0200

    add field

--- src/features/toolbox/add-tool.vue
+++ src/features/toolbox/add-tool.vue
@@ -4,12 +4,10 @@
     <div class="overlay" @click="tooglePopin()" :class="{ active: isPopin }"></div>
     <div class="add-tool__popin" :class="{ active: isPopin }">
       <div>
-        Nom
-        <input v-model="itemToPush.name" />
+        <input v-model="itemToPush.name" placeholder="Nom de l'outil" />
       </div>
       <div>
-        Lien
-        <input v-model="itemToPush.link" />
+        <input v-model="itemToPush.link" placeholder="Url de l'outils" />
       </div>
       <button @click="addTool()">Ajouter</button>
     </div>
@@ -98,8 +96,27 @@ export default {
     transition: all 200ms ease-out;
     padding: 15px;
 
+    display: flex;
+    align-items: center;
+    justify-content: center;
     &.active {
       transform: translateY(0px);
+    }
+
+    input {
+      margin-right: 10px;
+    }
+    button {
+      border: 0;
+      appearance: none;
+      background-color: #303f9f;
+      height: 42px;
+      padding: 5px 20px;
+      color: #fff;
+
+      &:hover {
+        background-color: darken(#303f9f, 10%);
+      }
     }
   }
 }
